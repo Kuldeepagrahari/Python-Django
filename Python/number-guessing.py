204a7ce161efc2233615cbaa0b3a9e2ac34cbf24
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2025-06-14 15:51:31 +0530

    Debug: Program was printing extra line, if user won

--- Python/number-guessing.py
+++ Python/number-guessing.py
@@ -3,19 +3,22 @@ import random
 systemNum = random.randint(1, 100)
 print("Hi! Hope you're doing well")
 print("Get-Set ready to guess the Number which system has generated!")
-print("You will have 6 chances to predict, otherwise you will lose")
+print("You will have 6 chances to predict, otherwise you will lose!")
 chanceNum = 0
 
 while(chanceNum < 6):
-    guessNum = int(input("guess: "))
-    chanceNum += 1
-    if(guessNum == systemNum):
-        print("Prediction is correct, You Won!")
-        break
-    elif(guessNum > systemNum):
-        print("Too High!")
-    elif(guessNum < systemNum):
-        print("Too Low!")
+    try:
+        guessNum = int(input("guess: "))
+        if(guessNum == systemNum):
+            print("Prediction is correct, You Won!")
+            break
+        elif(guessNum > systemNum):
+            print("Too High!")
+        elif(guessNum < systemNum):
+            print("Too Low!")
+        chanceNum += 1
+    except:
+        print("Invalid Input, please enter a valid Number")
 
 if(chanceNum == 6):
     print("You Tried well, better luck next time!")
